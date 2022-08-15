3162b82c48225c4d633eb00430740f20db27d530
InitSync <benjaalejandro206@gmail.com>
2022-08-15 12:51:39 -0400

    Moved the UserEntryEvent variable to method event.

--- src/main/java/me/proton/initsync/minijoin/listeners/PlayerJoinListener.java
+++ src/main/java/me/proton/initsync/minijoin/listeners/PlayerJoinListener.java
@@ -1,5 +1,6 @@
 package me.proton.initsync.minijoin.listeners;
 
+import me.clip.placeholderapi.PlaceholderAPI;
 import me.proton.initsync.minijoin.MiniJoin;
 import me.proton.initsync.minijoin.api.UserEntryEvent;
 import me.proton.initsync.minijoin.enums.Configuration;
@@ -20,11 +21,6 @@ public class PlayerJoinListener implements Listener
 	// A reference to the main class.
 	private final MiniJoin plugin;
 	
-	// A reference to the `UserEntryEvent` class.
-	private UserEntryEvent userEntryEvent;
-	// It's a list of actions that will be executed when the player joins.
-	private List<String> actions;
-	
 	// It's a constructor that is used to initialize the `plugin` variable.
 	public PlayerJoinListener(@NotNull MiniJoin plugin)
 	{
@@ -32,20 +28,32 @@ public class PlayerJoinListener implements Listener
 	}
 	
 	/**
-	 * It checks if the join-quit feature is enabled, then it gets the user's primary group, then it gets
-	 * the join message from the configuration, then it sets the join message, then it executes the join
-	 * actions
+	 * > When a player joins the server, the plugin checks if the join/quit messages are enabled, if they
+	 * are, it checks if the MOTD is enabled, if it is, it sends the MOTD to the player, then it checks if
+	 * the player's group is in the configuration, if it is, it sends the join message to the player and
+	 * executes the actions
 	 *
 	 * @param event The event that is being called.
 	 */
-	@EventHandler (priority = EventPriority.HIGH)
+	@EventHandler (priority = EventPriority.LOW)
 	public void onJoin(PlayerJoinEvent event)
 	{
 		final Player player = event.getPlayer();
 		
 		if (Configuration.check(Paths.JOIN_QUIT_ALLOW))
 		{
-			this.userEntryEvent = new UserEntryEvent();
+			final UserEntryEvent userEntryEvent = new UserEntryEvent();
+			
+			if (Configuration.check(Paths.JOIN_QUIT_ALLOW_MOTD))
+			{
+				Configuration.getList(Paths.JOIN_QUIT_MOTD)
+					 .forEach(string ->
+					 {
+						 player.sendMessage(Utils.miniMessage(player,
+							  PlaceholderAPI.setPlaceholders(player, string)
+						 ));
+					 });
+			}
 			
 			final String userGroup = this.plugin
 				 .luckPerms()
@@ -67,22 +75,23 @@ public class PlayerJoinListener implements Listener
 					 .getString("config.join-quit.groups." + userGroup + ".join");
 				assert joinMessage != null;
 				
-				this.userEntryEvent.joinMessage(joinMessage);
+				userEntryEvent.joinMessage(joinMessage);
 				event.joinMessage(Utils.miniMessage(player,
-					 this.userEntryEvent.joinMessage()
+					 PlaceholderAPI.setPlaceholders(player,
+						  userEntryEvent.joinMessage()
+					 )
 				));
 				
-				this.actions = this.plugin
+				final List<String> actions = this.plugin
 					 .configManager()
 					 .get("config.yml")
 					 .getStringList("config.join-quit.groups." + userGroup + ".join-actions");
-				this.actions
-					 .forEach(action ->
-					 {
-						 this.plugin
-								.actionHandler()
-								.execute(player, action);
-					 });
+				actions.forEach(action ->
+				{
+					this.plugin
+						 .actionHandler()
+						 .execute(player, action);
+				});
 			}
 		}
 	}
