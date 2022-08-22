47a86fdee3144852d7adc74b8333d2d5a671d698
Benjamín <benjaalejandro206@gmail.com>
2022-08-22 08:29:01 -0400

    Update build.gradle.kts

--- build.gradle.kts
+++ build.gradle.kts
@@ -18,28 +18,20 @@ repositories {
 
 dependencies {
 	compileOnly("io.papermc.paper:paper-api:1.18.2-R0.1-SNAPSHOT")
-	compileOnly("com.github.4drian3d:MiniPlaceholders:1.0.0")
 	compileOnly("me.clip:placeholderapi:2.11.2")
-	
-	implementation("com.github.MegavexNetwork.scoreboard-library:v1_18_R2:-SNAPSHOT")
-	implementation("com.github.MegavexNetwork.scoreboard-library:implementation:-SNAPSHOT")
 }
 
 tasks {
 	processResources {
 		expand(
-			 "name" to rootProject.name,
 			 "version" to version,
 			 "author" to "InitSync"
 		)
 	}
 	shadowJar {
-		archiveFileName.set("PaperBoard.jar")
-		duplicatesStrategy = DuplicatesStrategy.EXCLUDE
-		
-		relocate("net.megavex.scoreboardlibrary", "me.proton.initsync.minitab.libs.scoreboardlibrary")
+		archiveFileName.set("MiniJoin.jar")
+                destinationDirectory.set(file("$rootDir/bin/))
 	}
-	build {
-		dependsOn(shadowJar)
-	}
+
+	clean { delete("$rootDir/bin/) }
 }
